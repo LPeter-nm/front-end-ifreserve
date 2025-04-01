5d039e8ca646e8d60a92428794e21382e650618d
LPeter-nm <pg.nasmatos@outlook.com>
2025-04-01 11:13:43 -0300

    feat: adicionando pastas public-routes e private e finalizado paginas de login e escolha do tipo de usuário

--- src/app/(public-routes)/type-user/page.tsx
+++ src/app/(public-routes)/type-user/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/components/Footer/footer";
-import { LoginForm } from "@/components/FormLogin/login-form";
 import { TypeUserForm } from "@/components/FormTypeUser/typeuser-form";
 import Navbar from "@/components/NavBarPublic";
 import { Button } from "@/components/ui/button";
@@ -9,13 +8,13 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 w-full z-50">
         <Navbar rightContent={
-          <Button variant={'outline'} className="bg-[#2C2C2C] text-white w-24 border-0">Register</Button>
+          <Button variant={'outline'} className="bg-[#E3E3E3] text-black w-24">Login</Button>
         }/> 
       </div>
 
       <div className="flex-1 flex items-center justify-center pt-20"> 
         <div className="w-full max-w-md px-4">
-          <LoginForm/>
+          <TypeUserForm/>
         </div>
       </div>
       <Footer/>
