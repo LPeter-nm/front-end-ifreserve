1ccb47e2fb460bcf67c9766f4a545fe566c9a5c5
LPeter-nm <pg.nasmatos@outlook.com>
2025-04-01 11:14:45 -0300

    feat: componentes: [footer, formLogin, formTypeUser, navBarPublic]

---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import { Instagram, Linkedin } from "lucide-react"
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-[#00ff7f] py-5 px-6">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center">
+        <div className="border-l-2 border-black pl-4">
+          <p className="text-sm font-medium">Send e-mail</p>
+          <p className="text-sm font-medium">Lorem ipsum</p>
+          <p className="text-sm font-medium">Lorem ipsum v1.0</p>
+        </div>
+        <p className="text-sm mt-12">Copyright © 2023 Pedro Gabriel</p>
+
+        <div className="flex  mt-8">
+          <div className="flex space-x-2">
+            <Link href="#" aria-label="Instagram">
+              <Instagram size={20} className="text-black" />
+            </Link>
+            <Link href="#" aria-label="LinkedIn">
+              <Linkedin size={20} className="text-black" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
---
+++ src/components/FormTypeUser/typeuser-form.tsx
@@ -0,0 +1,60 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Label } from "@/components/ui/label"
+
+export function TypeUserForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6 ", className)} {...props}>
+      <Card className="bg-[#264543] border-0 text-white">
+        <CardHeader>
+          <CardTitle>Selecione seu tipo de usuário</CardTitle>
+          <CardDescription>
+            Escolha a opção que melhor descreve seu perfil
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="user-type">Tipo de usuário</Label>
+                <Select>
+                  <SelectTrigger id="user-type" className="w-full bg-white">
+                    <SelectValue placeholder="Selecione seu tipo" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="student">ALUNO</SelectItem>
+                    <SelectItem value="teacher">SERVIDOR</SelectItem>
+                    <SelectItem value="staff">EXTERNO</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              
+              <div className="flex flex-col gap-3 items-center">
+                <Button type="submit" className="w-48 bg-[#00FD94] text-black">
+                  Prosseguir
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
